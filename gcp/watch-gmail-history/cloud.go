8323ae4072d0a4bb070dd0689ae02a242fc459d4
yamamoto-tgz <yamamoto.tar.gz@gmail.com>
2025-02-15 15:26:44 +0900

    [Clean] oauth -> lib/oauth

--- gcp/watch-gmail-history/cloud.go
+++ gcp/watch-gmail-history/cloud.go
@@ -2,18 +2,12 @@ package watchgmailhistory
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 
-	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/cloudevents/sdk-go/v2/event"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/gmail/v1"
-	"google.golang.org/api/option"
 )
 
 var BUCKET_NAME string = os.Getenv("BUCKET_NAME")
@@ -42,7 +36,7 @@ func init() {
 }
 
 func watchGmailHistory(ctx context.Context, e event.Event) error {
-	srv, err := auth(ctx)
+	srv, err := Auth(ctx)
 	if err != nil {
 		return err
 	}
@@ -61,64 +55,4 @@ func watchGmailHistory(ctx context.Context, e event.Event) error {
 	fmt.Printf("HistoryId: %d\n", res.HistoryId)
 
 	return nil
-}
-
-func auth(ctx context.Context) (*gmail.Service, error) {
-	config, err := readAuthConfig(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	tkn, err := readAuthToken(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	cl := config.Client(ctx, tkn)
-	return gmail.NewService(ctx, option.WithHTTPClient(cl))
-}
-
-func readFileFromStorage(ctx context.Context, bucketName string, fileName string) (io.Reader, error) {
-	cl, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	obj := cl.Bucket(bucketName).Object(fileName)
-	r, err := obj.NewReader(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Close()
-
-	return r, nil
-}
-
-func readAuthConfig(ctx context.Context) (*oauth2.Config, error) {
-	r, err := readFileFromStorage(ctx, BUCKET_NAME, CREDENTIALS_JSON)
-	if err != nil {
-		return nil, err
-	}
-
-	bytes, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return google.ConfigFromJSON(bytes, gmail.GmailReadonlyScope)
-}
-
-func readAuthToken(ctx context.Context) (*oauth2.Token, error) {
-	r, err := readFileFromStorage(ctx, BUCKET_NAME, TOKEN_JSON)
-	if err != nil {
-		return nil, err
-	}
-
-	tkn := &oauth2.Token{}
-	err = json.NewDecoder(r).Decode(tkn)
-	if err != nil {
-		return nil, err
-	}
-
-	return tkn, nil
 }
--- lib/go.mod
+++ lib/go.mod
@@ -1,11 +1,10 @@
-module github.com/yamamoto-tgz/autosave
+module github.com/yamamoto-tgz/autosave/lib
 
 go 1.23.5
 
 require (
 	cloud.google.com/go/storage v1.50.0
-	github.com/GoogleCloudPlatform/functions-framework-go v1.9.1
-	github.com/cloudevents/sdk-go/v2 v2.15.2
+	golang.org/x/oauth2 v0.26.0
 	google.golang.org/api v0.221.0
 )
 
@@ -32,9 +31,6 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
@@ -46,12 +42,8 @@ require (
 	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.32.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
-	go.uber.org/atomic v1.4.0 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.10.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/oauth2 v0.26.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
